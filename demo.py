e942200693353e86f9362a0eceabad4aadd074d3
Sindhu1501 <sindhu.narasimhulu@gmail.com>
2021-05-29 19:22:49 +0530

    one line is added

--- demo.py
+++ demo.py
@@ -1,2 +1,21 @@
 print("hello")
 print("trying for the first time")
+
+
+def sort(nums):
+    for i in range(len(nums) - 1):
+        minPosition = i
+
+        for j in range(i, len(nums)):
+            if nums[j] < nums[minPosition]:
+                minPosition = j
+
+        temp = nums[i]
+        nums[i] = nums[minPosition]
+        nums[minPosition] = temp
+
+
+nums = [3, 6, 8, 3, 4, 6, 0]
+sort(nums)
+
+print(nums)
